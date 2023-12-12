139234e1d91542ade956f76b990bd4ad282098c7
Aria Chafai <aria2004@gmail.com>
2023-12-12 15:32:51 +0100

    v2

--- snake.py
+++ snake.py
@@ -1,9 +1,22 @@
 import pygame
+import random  
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
 
 pygame.init()
 
-screen = pygame.display.set_mode( (640, 480) )
+screen = pygame.display.set_mode( (400, 300) )
+pygame.draw.rect(screen, red, (50, 100, 100, 50))
 
+
+"""
+def affichage(L):
+    if 
+"""
 clock = pygame.time.Clock()
 
 while True:
@@ -16,3 +29,6 @@ while True:
     screen.fill( (0, 255, 0) )
 
     pygame.display.update()
+
+pygame.quit()
+
